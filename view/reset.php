f86d2741ad12334384c432e0b915c6e9b3b2f2e4
moreno-guillaume <moreno-guillaume@users.noreply.github.com>
2024-02-18 13:16:49 +0100

    nouvelle vue : reset
    Prend litteralement la place de l'ancienne page reset.php

---
+++ view/reset.php
@@ -0,0 +1,10 @@
+
+<?php 
+$nav = "reset";
+$titre="mot de passe oublié ?";
+include ( VIEW . "/header.php");
+include ( VIEW . "/metas.php");
+include ( VIEW . "/forms.php");
+include ( VIEW . "/footer.php");
+
+;?>
