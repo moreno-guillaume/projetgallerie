fb3a3181b4358f0c84905a66c414d2bf15ea58f2
moreno-guillaume <moreno-guillaume@users.noreply.github.com>
2024-02-18 21:24:49 +0100

    [réécriture : chemins vers mvc] [nouveau : autoload()]
    ajout d'un slash dans les define de model, view et controller
    Création de la fonction autoload qui lira les classes si elles sont appelées sans avoir besoin d'includes. Possible grâce à  spl_autoload_register(array(__CLASS__,'autoload'))

--- _config.php
+++ _config.php
@@ -1,22 +1,41 @@
-<?php 
+<?php
 
 ini_set("display_errors", "on");
 error_reporting(E_ALL);
 
 // echo '<pre>'; print_r($_SERVER); exit;
 
-$root = $_SERVER['DOCUMENT_ROOT'];
-$host = $_SERVER['HTTP_HOST'];
+class MyAutoLoad
+{
+     public static function start()
+    {
 
+        spl_autoload_register(array(__CLASS__,'autoload'));
+        // $root = $_SERVER['DOCUMENT_ROOT'];
+        $host = $_SERVER['HTTP_HOST'];
 
-define ('HOST', "http://" . $host.'/projetgallerie2/');
-define('ROOT', dirname(__FILE__));
+        define('HOST', "http://" . $host . '/projetgallerie2/');
+        define('ROOT', dirname(__FILE__));
 
+        define('CONTROLLER', ROOT . '/controller/');
+        define('VIEW',  ROOT . '/view/');
+        define('MODEL', ROOT . '/model/');
+        define('CLASSES', CONTROLLER . '_classes/');
+        // echo CLASSES; exit;
+        // echo MODEL; exit;
 
-define ('CONTROLLER', ROOT .'\controller');
-define ('VIEW',  ROOT .'\view');
-define ('MODEL', ROOT .'\model');
-define ('CLASSES', CONTROLLER . '\_classes');
+        define('ASSETS', HOST . 'assets/');
+    }
 
+    public static function autoload($class){
 
-define ('ASSETS', HOST.'assets/');
+        if(file_exists(MODEL . $class . '.php')){
+            include_once(MODEL. $class . '.php');
+        }elseif (file_exists( CLASSES. $class . '.php')){
+            include_once(CLASSES. $class . '.php');
+        }elseif (file_exists(CONTROLLER)){
+            include_once(CONTROLLER . $class . '.php');
+        }
+    }
+
+}
