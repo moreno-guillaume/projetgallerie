50ab70640c8fa0f48f174c6cd479d590a96e8912
moreno-guillaume <moreno-guillaume@users.noreply.github.com>
2024-02-26 20:54:14 +0100

    new : dropdown items, metas
    imported jquerry, + some extra bootrsap links allowing to show those dropdown-things on my nav

--- view/_gabarit.php
+++ view/_gabarit.php
@@ -14,9 +14,11 @@
     <!-- custom CSS -->
     <link rel="stylesheet" href="<?php echo ASSETS; ?>/css/home.css">
 
-    <!-- Bootsrap JS + popper -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
     <!-- custom JS -->
     <script src="assets/js/script.js" defer></script>
 
@@ -26,11 +28,11 @@
 <body>
 
 <!--------------- header -------------->
-<div class="container">
+<div class="container-fluid">
     <div class="row">
             <header class="mb-2 d-flex justify-content-center">
                 <div id="logo">
-                    <a href="index.php"><img src="./assets/img/logo.svg" alt="logo"></a>
+                    <a href="index.php"></a>
                 </div>
             </header>
         </div>
--- view/menu.php
+++ view/menu.php
@@ -1,11 +1,65 @@
-<?php if(isset($_SESSION["user"]["IdCollab"])):?>
-<nav>
-        <ul>
-                <li><a href="deconnexion.php">Deconnexion</a></li>
-                <li><a href="#"><img class="icon" src="./assets/img/icons/expo.svg" alt="Logo exposition">exposition</a></li>
-                <li><a href="#"><img class="icon" src="./assets/img/icons/oeuvre.svg" alt="Logo oeuvre">oeuvre</a></li>
-                <li><a href="#"><img class="icon" src="./assets/img/icons/calendar.svg" alt="Logo calendrier">Calendrier</a></li>
-                <li><a href="#"><img class="icon" src="./assets/img/icons/settings.svg" alt="Logo parametres">Parametres</a></li>
-        </ul>
-</nav>
-<?php endif ;?>
+<?php if (isset($_SESSION["user"]["IdCollab"])) : ?>
+        <nav>
+                <ul>
+                        <div class="dropdown">
+                                <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Expositions
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                        <a href="#" class="dropdown-item" type="button">Liste des Expos</a>
+                                        <a href="#" class="dropdown-item" type="button">Creer une nouvelle Expo</a>
+                                </div>
+                        </div>
+                        <div class="dropdown">
+                                <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Oeuvres
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                        <a href="#" class="dropdown-item" type="button">Liste des oeuvres</a>
+                                        <a href="#" class="dropdown-item" type="button">Enregistrer une nouvelle oeuvre</a>
+                                </div>
+                        </div>
+
+                        <div class="dropdown">
+                                <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Artistes
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                        <a href="#" class="dropdown-item" type="button">Liste des artistes</a>
+                                        <a href="#" class="dropdown-item" type="button">Enregistrer un nouvel artiste</a>
+                                </div>
+                        </div>
+
+                        <div class="dropdown">
+                                <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Traductions
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                        <a href="#" class="dropdown-item" type="button">Liste des traductions</a>
+                                        <a href="#" class="dropdown-item" type="button">Enregistrer un nouvel artiste</a>
+                                </div>
+                        </div>
+
+                        
+                        <?php if((isset($_SESSION["user"]["IdPoste"])&&($_SESSION["user"]["IdPoste"]=1))):?>
+                        <div class="dropdown">
+                                <button class="btn dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Collaborateurs
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                        <a href="#" class="dropdown-item" type="button">Liste des collaborateurs</a>
+                                        <a href="#" class="dropdown-item" type="button">Enregistrer un nouveau collaborateur</a>
+                                </div>
+                        </div>
+                        <?php endif ;?>
+
+                        <li class="dropdown"><a href="#" class="dropdown">Calendrier</a></li>
+                        
+                </ul>
+
+                <ul id="second-list">
+                        <li><a href="#">Parametres</a></li>
+                        <li><a href="deconnexion.php">Deconnexion</a></li>
+                </ul>
+        </nav>
+<?php endif; ?>
