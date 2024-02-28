627af1740c99270a2c734327693cbce464f1f55b
moreno-guillaume <moreno-guillaume@users.noreply.github.com>
2024-02-28 13:36:28 +0100

    nouveau : vue update.php
    Affichera, en fonction de la $nav, les informations concernant une expo, une oeuvre, etc.

---
+++ view/update.php
@@ -0,0 +1,17 @@
+<div class="container-fluid">
+    <?php $exposition = reset($data); ?>
+    <h3 class="text-center pt-5 pb-5 titre"><?php echo $exposition->getLibelle(); ?></h3>
+
+</div>
+<div class='row'>
+    <div class='col-1'></div>
+    <div class="col-5 form">
+        ppp
+    </div>
+    <div class="col-1"></div>
+    <div class="col-4 form">
+        ppp
+    </div>
+
+</div>
+</div>
