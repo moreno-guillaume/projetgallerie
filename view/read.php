3ac043d4f4a0d02fac855f84fc8046633bb9d64a
moreno-guillaume <moreno-guillaume@users.noreply.github.com>
2024-02-28 20:26:39 +0100

    Nouveau : vues de liste-oeuvres et voir-oeuvre

--- view/read.php
+++ view/read.php
@@ -4,7 +4,7 @@
         <div class="col-1"></div>
 
         <div class='col-10'>
-            <h3 class="text-center mt-5"><?php echo $titre; ?></h3>
+            <h2 class="text-center mt-5 pt-5 pb-5 titre"><?php echo $titre; ?></h2>
 
             <table id="matable" class="">
                 <thead class="">
@@ -19,13 +19,23 @@
                                     <th>Voir / Modifier</th>
                                     <th>Supprimer l'expo</th>";
                                 break;
+
+                            case "listeOeuvres":
+                                echo "<th>Nom de l'oeuvre</th>
+                                        <th>Année de création</th>
+                                        <th>Type d'oeuvre</th>
+                                        <th>Présente dans l'exposition : </th>
+                                        <th>Statut dans l'exposition : </th>
+                                        <th>Voir / Modifier</th>
+                                        <th>Supprimer l'oeuvre</th>";
+                                break;
                         }; ?>
                     </tr>
                 </thead>
                 <tbody>
-                        <?php if($nav=="listeExpos"){
+                    <?php if ($nav == "listeExpos") {
 
-                            foreach ($data as $exposition) : ?>
+                        foreach ($data as $exposition) : ?>
                             <tr>
                                 <td><?php echo $exposition->getLibelle(); ?></td>
                                 <td><?php echo $exposition->getStatut(); ?></td>
@@ -33,7 +43,25 @@
                                 <td><a href="voirExpo?id=<?php echo $exposition->getId(); ?>">Voir / modifier</a></td>
                                 <td><a href="#">Supprimer</a></td>
                             </tr>
-                        <?php endforeach; };?>
+                    <?php endforeach;
+                    }elseif($nav=="listeOeuvres"){
+
+                        foreach ($data as $oeuvre) : ?>
+                            <tr>
+                                <td><?php echo $oeuvre->getLibelle(); ?></td>
+                                <td class="text-center"><?php echo $oeuvre->getAnneeCreation(); ?></td>
+                                <td class="text-center"><?php echo $oeuvre->getType(); ?></td>
+                                <td class="text-center"><?php echo $oeuvre->getExpo(); ?></td>
+                                <td class="text-center"><?php echo $oeuvre->getStatut(); ?></td>
+                                <td><a href="voirOeuvre?id=<?php echo $oeuvre->getId(); ?>">Voir / modifier</a></td>
+                                <td><a href="#">Supprimer</a></td>
+                            </tr>
+                    <?php endforeach;
+                    }; ?>
+
+
+
+
         </div>
     </div>
 </div>
--- view/update.php
+++ view/update.php
@@ -1,16 +1,58 @@
 <div class="container-fluid">
     <?php $exposition = reset($data); ?>
-    <h3 class="text-center pt-5 pb-5 titre"><?php echo $exposition->getLibelle(); ?></h3>
+    <h2 class="text-center pt-5 pb-5 titre"><?php echo $exposition->getLibelle(); ?></h2>
 
 </div>
 <div class='row'>
     <div class='col-1'></div>
-    <div class="col-5 form">
-        ppp
+
+    <div class="col-lg-5 col-sm-10 p-5  form">
+
+    <h3 class='text-center titre pt-2 pb-5'>Infos</h3>
+
+        <form action="" method="" class="mb-2 p-3 form grid-3 d-flex justify-content-between bg-light">
+            <label for="Nom" class="my-auto">Nom : </label>
+            <p class="text-uppercase my-auto"><?php echo $exposition->getLibelle(); ?></p>
+            <button type="button" class='btn sm-btn border'>Modifier</button>
+        </form>
+
+        <form action="" method="" class="mb-2 p-3 form grid-3 d-flex justify-content-between bg-light">
+            <label for="date-debut" class="my-auto">Date de début : </label>
+            <p class="my-auto"><?php echo $exposition->getDateDebut(); ?></p>
+            <button type="button" class='btn sm-btn border'>Modifier</button>
+        </form>
+
+        <form action="" method="" class="mb-2 p-3 form grid-3 d-flex justify-content-between bg-light">
+            <label for="Email" class="my-auto">Date de fin : </label>
+            <p class="my-auto"><?php echo $exposition->getDateFin(); ?></p>
+            <button type="button" class='btn sm-btn border'>Modifier</button>
+        </form>
+
+        <form action="" method="" class="mb-2 p-3 form grid-3 d-flex justify-content-between bg-light">
+            <label for="Nom" class="my-auto">Statut : </label>
+            <p class="my-auto"><?php echo $exposition->getStatut(); ?></p>
+            <button type="button" class='btn sm-btn border'>Modifier</button>
+        </form>
+
+        <form action="" method="" class="mb-2 p-3 form grid-3 d-flex justify-content-between bg-light">
+            <label for="Nom" class="my-auto">Thème : </label>
+            <p class="my-auto"><?php echo $exposition->getTheme(); ?></p>
+            <button type="button" class='btn sm-btn border'>Modifier</button>
+        </form>
     </div>
-    <div class="col-1"></div>
-    <div class="col-4 form">
-        ppp
+
+    <div class="col-sm-1"></div>
+
+    <div class="col-lg-4 col-sm-10 form p-5">
+
+    
+
+        <h3 class='text-center titre pt-2 pb-5'>Description</h3>
+
+        <form action="">
+            <textarea class="description p-4"><?php echo $exposition->getDescription(); ?></textarea>
+            <button type="button" class='btn sm-btn border float-end mt-5 bg-light'>Enregistrer les modifications</button>
+        </form>
     </div>
 
 </div>
