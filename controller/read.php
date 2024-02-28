7752eb784436fba7a5e6c97cd9be765ff333db48
moreno-guillaume <moreno-guillaume@users.noreply.github.com>
2024-02-28 20:25:49 +0100

    Update : controller read.php
    Apelle d'une nouvelle vue si $nav == listeOeuvres

--- controller/read.php
+++ controller/read.php
@@ -6,22 +6,25 @@ class Read
     public function showList($nav)
     {
 
-        if ($nav == "listeExpos") {
-            $titre = "Liste des expositions";
-        }
 
         if (isset($_SESSION["user"]["IdCollab"])) {
-            include(VIEW . "menu.php");
+        include(VIEW . "menu.php");
         }
-
-        $manager = new ExpositionManager();
-        $expositions = $manager->findAll();
-        $data = $expositions;
-
 
         if ($nav == "listeExpos") {
             $titre = "Liste des expositions";
+            $manager = new ExpositionManager();
+            $expositions = $manager->findAll();
+            $data = $expositions;
+            
+
+        }elseif($nav=='listeOeuvres'){
+            $titre = "Liste des oeuvres";
+            $manager = new OeuvreManager();
+            $oeuvres = $manager->findAll();
+            $data = $oeuvres;
         }
+
 
         $myView = new View('read');
         $myView->render($titre, $nav, $data);
