22f7b74091efc8bbeadb815d5eba40ec4713a70c
moreno-guillaume <moreno-guillaume@users.noreply.github.com>
2024-02-29 16:00:43 +0100

    modification : view / update
    La view update affiche maintenant une div consacrée à l'image liée au contenu séléctionné, + une div .hidden appelée sur un click d'un des boutons traduction présents dans la "description"

--- view/update.php
+++ view/update.php
@@ -20,7 +20,7 @@
             case "voirOeuvre":
                 echo $oeuvre->getLibelle();
                 break;
-            }?>
+        } ?>
     </h2>
 
 </div>
@@ -92,32 +92,52 @@
                     break;
                 case "voirOeuvre":
                     echo "<label for='statut' class='my-auto'>Statut : </label>";
-                    echo "<p class='my-auto'>" . $oeuvre->getNomArtiste() . "</p>";
+                    echo "<p class='my-auto'>" . $oeuvre->getStatut() . "</p>";
                     echo "<button type='button' class='btn sm-btn border'>Modifier</button>";
                     break;
             } ?>
         </form>
 
-        
-            <?php
-            switch ($nav) {
-                case "voirExpo":
-                    echo "<form action='' method='' class='mb-2 p-3 form grid-3 d-flex justify-content-between bg-light'>";
-                    echo "<label for='theme' class='my-auto'>Thème : </label>";
-                    echo "<p class='my-auto'>" . $exposition->getTheme() . "</p>";
-                    echo "<button type='button' class='btn sm-btn border'>Modifier</button>";
-                    echo "</form>";
-                    break;
-            } ?>
-        
+
+        <?php
+        switch ($nav) {
+            case "voirExpo":
+                echo "<form action='' method='' class='mb-2 p-3 form grid-3 d-flex justify-content-between bg-light'>";
+                echo "<label for='theme' class='my-auto'>Thème : </label>";
+                echo "<p class='my-auto'>" . $exposition->getTheme() . "</p>";
+                echo "<button type='button' class='btn sm-btn border'>Modifier</button>";
+                echo "</form>";
+                break;
+        } ?>
 
     </div>
 
     <div class="col-sm-1"></div>
 
     <div class="col-lg-4 col-sm-10 form p-5">
+        <h3 class='text-center titre pt-2 pb-5'>image</h3>
+        <div class="img-container bg-info">
+            <form action="">
+                <?php
+                switch ($nav) {
+                    case "voirExpo":
+                        echo "<img src=" . $exposition->getImage() . ">";
+                        break;
+                } ?>
+        </div>
+        <button type='button' class='btn sm-btn border bg-light mt-5 float-end'>Modifier</button>
+        </form>
+    </div>
 
+    <div class="col-1"></div>
 
+</div>
+
+<div class='row'>
+
+    <div class='col-1'></div>
+
+    <div class="col-lg-5 mt-5 mb-5 col-sm-10 p-5 form">
 
         <h3 class='text-center titre pt-2 pb-5'>Description</h3>
 
@@ -126,14 +146,51 @@
             switch ($nav) {
                 case "voirExpo":
                     echo "<textarea class='description p-4'>" . $exposition->getDescription() . "</textarea>";
-                    echo "<button type='button' class='btn sm-btn border float-end mt-5 bg-light'>Enregistrer les modifications</button>";
+                    echo "<div class='d-flex justify-content-between'>";
+                    echo "<button type='button' class='btn sm-btn border mt-5 bg-light'>Enregistrer les modifications</button>";
+                    echo "</div>";
                     break;
                 case "voirOeuvre":
                     echo "<textarea class='description p-4'>" . $oeuvre->getDescription() . "</textarea>";
                     echo "<button type='button' class='btn sm-btn border float-end mt-5 bg-light'>Enregistrer les modifications</button>";
                     break;
-            }
-            ?>
+            } ?>
+        </form>
+
+        <div class="dropdown">
+            <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                traduire ce document
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <button class="dropdown-item" type="button" value="Anglais" onclick="showTraduction('en')">Anglais</button>
+                <button class="dropdown-item" type="button" value="Espagnol" onclick="showTraduction('es')">Espagnol</button>
+                <button class="dropdown-item" type="button" value="Allemand" onclick="showTraduction('all')">Allemand</button>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col-1"></div>
+
+    <div id="traduction-container" class="col-lg-4 mt-5 mb-5 col-sm-10 p-5 form hidden">
+
+        <h3 class='text-center titre pt-2 pb-5'><span>Traduction</span></h3>
+
+        <form id='formulaire-traduction' action="">
+
+    <textarea class='description p-4'>
+    <?php
+            switch ($nav) {
+                case "voirExpo":
+                    echo $exposition->getDescription();
+                    break;
+                case "voirOeuvre":
+                    echo $oeuvre->getDescription();
+                    break;
+            } ?>
+
+    </textarea>
+            <button type='button' class='btn sm-btn border bg-light mt-5 float-end' id="envoyer-traduction">Enregistrer la traduction</button>
         </form>
     </div>
 
