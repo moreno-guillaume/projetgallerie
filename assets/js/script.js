98b6f586645c034f30b691fe84408e33bd1c8eae
moreno-guillaume <moreno-guillaume@users.noreply.github.com>
2024-02-29 15:55:25 +0100

    nouveau : fonction showTraduction ()
    Permet, au click d'un des boutons de séléction de langue, d'afficher une div initialement .hidden. La fonction prend un language en argument et set l'id du formulaire en donction de celui-ci

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
 let table = new DataTable("#matable");
+
+
+    function showTraduction(language) {
+        const traductionContainer = document.getElementById("traduction-container");
+        const formulaireTraduction = document.getElementById("formulaire-traduction");
+        const titreSpan = document.querySelector('.titre span');
+        const envoyerTrad = document.getElementById('envoyer-traduction');
+        
+        if(!traductionContainer.classList.contains('hidden')){
+        traductionContainer.classList.add('hidden');
+        traductionContainer.classList.remove('hidden');
+        }
+
+    
+        switch (language) {
+            case 'en':
+                titreSpan.textContent = 'Translation';
+                envoyerTrad.textContent = 'Save translation';
+                break;
+            case 'es':
+                titreSpan.textContent = 'traducción';
+                envoyerTrad.textContent = 'guardar traducción';
+                break;
+            case 'all':
+                titreSpan.textContent = 'Übersetzung';
+                envoyerTrad.textContent = 'Übersetzung speichern';
+                break;
+        }
+    
+        traductionContainer.classList.remove('hidden');
+        
+        formulaireTraduction.setAttribute("data-lang", language);
+    }
